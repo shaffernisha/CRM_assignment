c727f47fe975e8e9e0dbe28866677d20d9b25904
shaffernisha <shaffernisha96@gmail.com>
2026-02-03 11:24:52 +0530

    Update CORS origin for deployment URL

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: ["http://localhost:5173", "https://your-app-name.vercel.app"],
+  origin: ["http://localhost:5173", "https://crm-assignment-xi.vercel.app/"],
   credentials: true
 }));
 
